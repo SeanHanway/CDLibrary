981bc16de2f0f522e302a344800ff3c3d3c623d8
Seán Hanway <seanhanway@yahoo.com>
2016-06-25 15:58:31 +0100

    Documentation. Small change to handleSubmitOkButton method

--- src/main/java/com/yahoo/seanhanway/CDLibraryFX/NewLibraryDialogueWindowController.java
+++ src/main/java/com/yahoo/seanhanway/CDLibraryFX/NewLibraryDialogueWindowController.java
@@ -1,6 +1,5 @@
 package com.yahoo.seanhanway.CDLibraryFX;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
@@ -9,13 +8,16 @@ public class NewLibraryDialogueWindowController extends CDLibraryFXController{
     @FXML private TextField libraryname;
     @FXML private Text warning;
 
-    @FXML protected void handleSubmitOKButton(ActionEvent event){
+    /**
+     * Handles pressing the "OK" button within the popup dialogue window.
+     */
+    @FXML protected void handleSubmitOKButton(){
         if(helper.addCDLibrary(libraryname.getCharacters().toString())){
+            CDLibraryFXController.setCDLibraryName(libraryname.getCharacters().toString());
             closeStage();
         }
         else{
             warning.setText("Library already exists.");
         }
     }
-
 }
