ff8ad8c253a44bcb657874b00c9f02dc311c0537
Seán Hanway <seanhanway@yahoo.com>
2016-06-26 13:05:09 +0100

    Functionality for the Open Library dialogue window is now fully implemented.

--- src/main/java/com/yahoo/seanhanway/CDLibraryFX/OpenLibraryDialogueWindowController.java
+++ src/main/java/com/yahoo/seanhanway/CDLibraryFX/OpenLibraryDialogueWindowController.java
@@ -3,16 +3,34 @@ package com.yahoo.seanhanway.CDLibraryFX;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 
-public class OpenLibraryDialogueWindowController extends CDLibraryFXController {
-    @FXML protected ListView<String> cdlibrarylist = new ListView<>();
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class OpenLibraryDialogueWindowController extends CDLibraryFXController implements Initializable {
+    @FXML protected ListView<String> cdlibrarylist;
 
     /**
-     * Constructor - Initialises and populates the ListView.
+     * Populates the list within the OpenLibraryDialogueWindow with the name of CDLibrary objects found on the disk and created during the current user session.
+     * @param location N/A
+     * @param resources N/A
      */
-    public OpenLibraryDialogueWindowController(){
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
         ObservableList<String> names = FXCollections.observableArrayList(helper.getLibraryAsString());
         cdlibrarylist.setItems(names);
     }
+
+    /**
+     * Sets the String CDLibraryName in the CDLibraryFXController as the name of the selected CDLibrary and closes the stage.
+     */
+    @FXML protected void handleSubmitOKButton(){
+        if(cdlibrarylist.getSelectionModel().getSelectedItem()!=null) {
+            setCDLibraryName(cdlibrarylist.getSelectionModel().getSelectedItem());
+            closeStage();
+        }
+    }
+
 }
